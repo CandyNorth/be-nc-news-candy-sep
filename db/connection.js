ff1c5d22624aa736a6caa139642cf7cef275bc74
CandyNorth <candydaviesnorth@gmail.com>
2024-10-18 15:10:40 +0100

    add hosting changes to connection, and package with new listen file

--- db/connection.js
+++ db/connection.js
@@ -1,13 +1,13 @@
-const { Pool } = require('pg');
-const ENV = process.env.NODE_ENV || 'development';
+const { Pool } = require("pg");
+const ENV = process.env.NODE_ENV || "development";
 const config = {};
 
-require('dotenv').config({
+require("dotenv").config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
-  throw new Error('PGDATABASE and DATABASE_URL not set');
+  throw new Error("PGDATABASE and DATABASE_URL not set");
 }
 
 if (ENV === "development") {
@@ -16,6 +16,11 @@ if (ENV === "development") {
 }
 
 if (ENV === "test") {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
+}
+
+if (ENV === "production") {
   config.connectionString = process.env.DATABASE_URL;
   config.max = 2;
 }
